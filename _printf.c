dbe5c80dd57c3b24722a84ba135162cc364ea78c
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-07-12 12:23:26 -0700

    Printf self written function

--- _printf.c
+++ _printf.c
@@ -8,16 +8,40 @@
 
 int _printf(const char *format, ...)
 {
-int i; /* Counter for char*/
+char special[] = {'c', 's', 'd', 'i', 'o', 'x', 'X', 'p', 'b', 'S', 'r', 'R'};
+int i, x, s = 0; /* Counter for char*/
 va_list arg_list; /* Using ellips, use list to use as placeholder*/
 
 va_start(arg_list, format);/*Initializes the arg list*/
-for (i = 0; format[i]; i++)
+for (i = 0; format[i] != '\0'; i++)
 {
-	char c = va_arg(arg_list, int);
-	printf("%c,%d", c, i);
+		if (format == NULL)
+			return (NULL);
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '%')
+			{	
+				_putchar('%');
+				continue;
+			}
+			for (x = 0; x <= 11; x ++)
+			{
+				if (format[i] == special[x])
+				{
+					s += input_mod(format[i]);
+					continue;
+				}
+				else
+				{
+					x++
+				}
+			}
+		}
+	s += _putchar(format[i]);
+char c = va_arg(arg_list, int);
+}
 va_end(arg_list); /* Ends the va list */
-return (i);
+return (s);
 }
 return (0);
 }
